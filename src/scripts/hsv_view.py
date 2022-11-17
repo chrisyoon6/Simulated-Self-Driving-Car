ada2ccfcfdf0b83b0a435c5726d3cb8ca9bd6d9f
Chris Yoon <chris7yoon@gmail.com>
2022-11-16 19:12:00 -0800

    clean up

--- src/scripts/hsv_view.py
+++ src/scripts/hsv_view.py
@@ -12,12 +12,13 @@ import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-uh = 179
-us = 10
-uv = 255
-lh = 0
-ls = 0
-lv = 200
+red_low = [0, 50, 50]
+red_up = [10, 255, 255]
+blue_low = [110, 50, 50]
+blue_up = [130, 255, 255]
+white_low = [0, 0, 200]
+white_up = [179, 10, 255]
+
 lower_hsv = np.array([lh,ls,lv])
 upper_hsv = np.array([uh,us,uv])
 
@@ -26,20 +27,23 @@ class image_converter:
   def __init__(self):
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,self.callback)
-    self.i = 0
-
+    """Currently filtered images"""    
+    self.blue_im = None
+    self.red_im = None
+    self.white_im = None
 
   def process_image(self,image):
-    #image processing
-    # image[:,:,2] = 0
-    # image[:,:,1] = 0
-
+    """Filters an image to show: blue, red, white only, respectively. Updates this object"""
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
+    self.blue_im = filter(hsv, blue_low, blue_up)
+    self.red_im = filter(hsv, red_low, red_up)
+    self.white_im = filter(hsv, white_low, white_up)
+
+  def filter(self, image, hsv_low, hsv_up):
+    """Filters the image to the hsv ranges specified"""
+    mask = cv2.inRange(image, hsv_low, hsv_up)
     blur = cv2.GaussianBlur(mask,(3,3), 0)
-
     return blur
-
     
   def callback(self,data):
     try:
@@ -47,22 +51,9 @@ class image_converter:
     except CvBridgeError as e:
       print(e)
 
-    processed_im = self.process_image(cv_image)
-
-    # draw contours on the original image
-    # contours, hierarchy = cv2.findContours(image=processed_im, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
-    # largest_item= sorted(contours, key=cv2.contourArea, reverse= True)[0]
-    # M = cv2.moments(largest_item)
-
-    # cx = int(M['m10']/M['m00'])
-    # cy = int(M['m01']/M['m00'])
-
-    # disp = cv2.circle(processed_im, (cx, cy), 2, (0,255,0), 2)
-    # cv2.drawContours(image=disp, contours=contours, contourIdx=0, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
-    # see the results
-    #self.i+=1
-    #w_title = ("none {}".format(self.i))
-    cv2.imshow('script_view', processed_im)
+    self.process_image(cv_image)
+    
+    cv2.imshow('script_view', self.blue_im)
     cv2.waitKey(3)
 
 
--- src/scripts/scrape_data.py
+++ src/scripts/scrape_data.py
@@ -13,22 +13,21 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Twist
 
+WIDTH, HEIGHT = (1280, 720)
+FPS = 20
 class data_scraper:
-
     def __init__(self) -> None:
         self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,self.callback)
         self.bridge = CvBridge()
         
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-        self.video_writer = cv2.VideoWriter('test_data.mp4', fourcc, 20, (1280, 720))
+        self.video_writer = cv2.VideoWriter('test_data.mp4', fourcc, FPS, (WIDTH, HEIGHT))
         # print("video writer opened: %b", self.video_writer.isOpened())
-
 
     def callback(self, data):
         cv_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
         frame = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
         self.video_writer.write(frame)
-
 
 def main(args):
   ds = data_scraper()
