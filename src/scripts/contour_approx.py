d44b1ef2837869d85e5720a91fec5dde6407fbd8
julianLapenna12 <7jlapenna@gmail.com>
2022-11-16 17:30:31 -0800

    added contour approx

--- src/scripts/contour_approx.py
+++ src/scripts/contour_approx.py
@@ -12,21 +12,12 @@ import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-<<<<<<< HEAD
-uh = 130
-us = 255
-uv = 255
-lh = 110
-ls = 50
-lv = 50
-=======
 uh = 179
 us = 10
-uv = 255
+uv = 230
 lh = 0
 ls = 0
-lv = 200
->>>>>>> julian1
+lv = 150
 lower_hsv = np.array([lh,ls,lv])
 upper_hsv = np.array([uh,us,uv])
 
@@ -40,22 +31,14 @@ class image_converter:
 
   def process_image(self,image):
     #image processing
-<<<<<<< HEAD
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.medianBlur(gray,5)
-    ret,th = cv2.threshold(blur,100,255,cv2.THRESH_BINARY_INV)
-
-    return th
-=======
     # image[:,:,2] = 0
     # image[:,:,1] = 0
 
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
-    blur = cv2.GaussianBlur(mask,(3,3), 0)
+    blur = cv2.GaussianBlur(mask,(7,7), 0)
 
     return blur
->>>>>>> julian1
 
     
   def callback(self,data):
@@ -64,38 +47,34 @@ class image_converter:
     except CvBridgeError as e:
       print(e)
 
+    out = cv_image.copy()
     processed_im = self.process_image(cv_image)
 
     # draw contours on the original image
-<<<<<<< HEAD
-    contours, hierarchy = cv2.findContours(image=processed_im, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
-    largest_item= sorted(contours, key=cv2.contourArea, reverse= True)[0]
-    M = cv2.moments(largest_item)
 
+    # gets the contours in the thresholded image
+    contours, hierarchy = cv2.findContours(image=processed_im, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+
+    #gets the biggest contour and its info
+    c = max(contours, key=cv2.contourArea)
+    M = cv2.moments(c)
+
+    # gets the center of mass of the contour
     cx = int(M['m10']/M['m00'])
     cy = int(M['m01']/M['m00'])
 
-    disp = cv2.circle(processed_im, (cx, cy), 2, (0,255,0), 2)
-    cv2.drawContours(image=disp, contours=contours, contourIdx=0, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
-    # see the results
-    #self.i+=1
-    #w_title = ("none {}".format(self.i))
-    cv2.imshow('a', disp)
-=======
-    # contours, hierarchy = cv2.findContours(image=processed_im, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
-    # largest_item= sorted(contours, key=cv2.contourArea, reverse= True)[0]
-    # M = cv2.moments(largest_item)
+    # draws a circle at the center of mass of contour
+    disp = cv2.circle(out, (cx, cy), 2, (0,255,0), 2)
 
-    # cx = int(M['m10']/M['m00'])
-    # cy = int(M['m01']/M['m00'])
+    # approximates the contour to a simpler shape
+    epsilon = 0.1 #higher means simplify more
+    perimiter = cv2.arcLength(c,True)
+    approx = cv2.approxPolyDP(c,epsilon*perimiter,True)
 
-    # disp = cv2.circle(processed_im, (cx, cy), 2, (0,255,0), 2)
-    # cv2.drawContours(image=disp, contours=contours, contourIdx=0, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
-    # see the results
-    #self.i+=1
-    #w_title = ("none {}".format(self.i))
-    cv2.imshow('script_view', processed_im)
->>>>>>> julian1
+
+    cv2.drawContours(image=disp, contours=[approx], contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+ 
+    cv2.imshow('script_view', disp)
     cv2.waitKey(3)
 
 
