0419f8409095d9bbe44a59e8f0d0bc8ee6914b31
julianLapenna12 <7jlapenna@gmail.com>
2022-11-23 12:50:22 -0800

    master script completes sim reqs

--- src/scripts/master.py
+++ src/scripts/master.py
@@ -13,33 +13,35 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 
 
-class image_converter:
+class master:
 
   def __init__(self):
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,self.callback)
-    self.clock_sub = rospy.Subscriber("/clock")
+    self.clock_sub = rospy.Subscriber("/clock", Image)
 
-    self.vel_pub = rospy.Publisher("/R1/cmd_vel", Twist)
-    self.license_pub = rospy.Publisher("/license_plate", String)
+    self.vel_pub = rospy.Publisher("/R1/cmd_vel", Twist, queue_size=1)
+    self.license_pub = rospy.Publisher("/license_plate", String, queue_size=1)
 
-    self.out_plate = String
-    self.out_vel = Twist
+    self.out_plate = String()
+    self.out_vel = Twist()
     self.counter = 0
 
     
   def callback(self,data):
 
     if (self.counter == 0):
-        self.license_pub.publish(str('TeamYoonifer,multi21,0,AA00'))
+        self.license_pub.publish(String('TeamYoonifer,multi21,0,AA00'))
+    elif (self.counter == 20):
+        self.license_pub.publish(String('TeamYoonifer,multi21,-1,AA00'))
     else:
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
 
-        self.out_vel.linear.x = 0.4
-        self.out_vel.angular.z = 0
+        self.out_vel.linear.x = 0
+        self.out_vel.angular.z = 0.4
 
         self.vel_pub.publish(self.out_vel)
 
@@ -49,8 +51,8 @@ class image_converter:
 
 
 def main(args):
-  ic = image_converter()
-  rospy.init_node('image_converter', anonymous=True)
+  m = master()
+  rospy.init_node('master', anonymous=True)
   try:
     rospy.spin()
   except KeyboardInterrupt:
