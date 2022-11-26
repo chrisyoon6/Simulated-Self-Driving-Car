de9df30b04e1591c5afcb0bdbc3d0b35aa667647
julianLapenna12 <7jlapenna@gmail.com>
2022-11-25 16:36:00 -0800

    issues w model

--- src/scripts/char_reader.py
+++ src/scripts/char_reader.py
@@ -7,44 +7,63 @@ from concurrent.futures import process
 import sys
 import rospy
 import cv2
-import time
 import numpy as np
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
 
-from tensorflow.keras import layers
+import tensorflow as tf
 from tensorflow.keras import models
-from tensorflow.keras import optimizers
-
 from tensorflow.keras.utils import plot_model
-from tensorflow.keras import backend
+from PIL import Image
+
 
 class char_reader:
-    def __init__(self):
+    def __init__(self, path):
+        self.model = models.load_model(path)
+        print(type(self.model))
 
-        self.model_path = 
-        self.model = tf.keras.models.load_model(self.model_path, custom_objects={'CustomLayer': CustomLayer})
-        self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,self.callback)
-        self.i = 0
+    def predict(self, img, debug=False):
+        """Returns the model predicted character for a given image.
+        If `debug=True` it returns the probability too."""
+
+        img_aug = np.expand_dims(img, axis=0)
+        y_predict = self.model.predict(img_aug)[0]
+
+        if np.argmax(y_predict) < 26:
+            char = chr(np.argmax(y_predict)+ord("A"))
+        else:
+            char = chr(np.argmax(y_predict)-26+ord("0"))
+
+        if debug:
+            prob = y_predict[np.argmax(y_predict)]
+            return char, prob
+
+        return char
+
+    def model_summary(self):
+        """Returns model summary"""
+        return self.model.summary()
 
 
-    def get_config(self):
-        pass
-
-
-    def from_config(cls, config):
-        pass
-
-
-    
 def main(args):
-  cr = char_reader()
-  rospy.init_node('char_reader', anonymous=True)
-  try:
-    rospy.spin()
-  except KeyboardInterrupt:
-    print("Shutting down")
-  cv2.destroyAllWindows()
+    path = '/home/fizzer/ros_ws/src/ENPH353-Team12/src/models/license_plate_model1.h5'
+    cr = char_reader(path)
+
+    input = np.array(Image.open('/home/fizzer/ros_ws/src/ENPH353-Team12/src/license-plate-data/test_char_Y.png'))
+    input = input/255
+    input = np.expand_dims(input, axis=0)
+
+    print(input.shape)
+    print(cr.predict(input))
+
+    rospy.init_node('char_reader', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
-    main(sys.argv)
+    path = '/home/fizzer/ros_ws/src/ENPH353-Team12/src/models/license_plate_model1.h5'
+    model = models.load_model(path)
+
+    # main(sys.argv)
--- src/scripts/contour_approx.py
+++ src/scripts/contour_approx.py
@@ -7,10 +7,10 @@ from concurrent.futures import process
 import sys
 import rospy
 import cv2
-import time
 import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from char_reader import char_reader
 
 uh = 179
 us = 10
@@ -18,8 +18,8 @@ uv = 210
 lh = 0
 ls = 0
 lv = 90
-lower_hsv = np.array([lh,ls,lv])
-upper_hsv = np.array([uh,us,uv])
+lower_hsv = np.array([lh, ls, lv])
+upper_hsv = np.array([uh, us, uv])
 
 CAR_WIDTH = 200
 CAR_HEIGHT = 320
@@ -30,166 +30,175 @@ PLATE_RES = (150, 298)
 font = cv2.FONT_HERSHEY_COMPLEX
 font_size = 0.5
 
+
 class image_converter:
 
-  def __init__(self):
-    self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,self.callback)
-    self.i = 0
+    def __init__(self):
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber(
+            "/R1/pi_camera/image_raw", Image, self.callback)
+        #self.char_reader = char_reader()
+        self.i = 0
 
+    def process_stream(self, image):
+        """processes the image using a grey filter to catch license plates
 
-  def process_stream(self,image):
-    """processes the image using a grey filter to catch license plates
-    
-    returns a cv image"""
-    #image processing
-    # image[:,:,2] = 0
-    # image[:,:,1] = 0
+        returns a cv image"""
 
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
-    blur = cv2.GaussianBlur(mask,(5,5), 0)
-    dil = cv2.dilate(blur, (5,5))
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
+        blur = cv2.GaussianBlur(mask, (5, 5), 0)
+        dil = cv2.dilate(blur, (5, 5))
 
-    return dil
+        return dil
 
-    
-  def callback(self,data):
-    try:
-      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-    except CvBridgeError as e:
-      print(e)
+    def callback(self, data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
 
-    out = cv_image.copy()
-    processed_im = self.process_stream(cv_image)
+        out = cv_image.copy()
+        processed_im = self.process_stream(cv_image)
 
-    # draw contours on the original image
+        # draw contours on the original image
 
-    # gets the contours in the thresholded image
-    contours, hierarchy = cv2.findContours(image=processed_im, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+        # gets the contours in the thresholded image
+        contours, hierarchy = cv2.findContours(
+            image=processed_im, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
 
-    #gets the biggest contour and its info
-    c = max(contours, key=cv2.contourArea)
-    M = cv2.moments(c)
+        # gets the biggest contour and its info
+        c = max(contours, key=cv2.contourArea)
+        M = cv2.moments(c)
 
-    # gets the center of mass of the contour
-    cx = int(M['m10']/M['m00'])
-    cy = int(M['m01']/M['m00'])
+        # gets the center of mass of the contour
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
 
-    # draws a circle at the center of mass of contour
-    disp = cv2.circle(out, (cx, cy), 2, (0,255,0), 2)
+        # draws a circle at the center of mass of contour
+        disp = cv2.circle(out, (cx, cy), 2, (0, 255, 0), 2)
 
-    # approximates the contour to a simpler shape
-    epsilon = 0.1 #higher means simplify more
-    perimiter = cv2.arcLength(c,True)
-    approx = cv2.approxPolyDP(c,epsilon*perimiter,True)
+        # approximates the contour to a simpler shape
+        epsilon = 0.1  # higher means simplify more
+        perimiter = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, epsilon*perimiter, True)
 
-    n = approx.ravel()
-    pts = np.float32(self.get_coords(n)).reshape(-1, 2)
-    sorted_pts = self.contour_coords_sorted(pts)
-    
-    cv2.putText(disp, "tl", (int(sorted_pts[0][0]), int(sorted_pts[0][1])), font, font_size, (0, 255, 0)) 
-    cv2.putText(disp, "tr", (int(sorted_pts[1][0]), int(sorted_pts[1][1])), font, font_size, (0, 255, 0)) 
-    cv2.putText(disp, "bl", (int(sorted_pts[2][0]), int(sorted_pts[2][1])), font, font_size, (0, 255, 0)) 
-    cv2.putText(disp, "br", (int(sorted_pts[3][0]), int(sorted_pts[3][1])), font, font_size, (0, 255, 0)) 
-    # print(pts)
+        n = approx.ravel()
+        pts = np.float32(self.get_coords(n)).reshape(-1, 2)
+        sorted_pts = self.contour_coords_sorted(pts)
 
-    # resizing to have pairs of points
-    plate_view = self.transform_perspective(CAR_WIDTH, CAR_HEIGHT, sorted_pts, out)
+        cv2.putText(disp, "tl", (int(sorted_pts[0][0]), int(
+            sorted_pts[0][1])), font, font_size, (0, 255, 0))
+        cv2.putText(disp, "tr", (int(sorted_pts[1][0]), int(
+            sorted_pts[1][1])), font, font_size, (0, 255, 0))
+        cv2.putText(disp, "bl", (int(sorted_pts[2][0]), int(
+            sorted_pts[2][1])), font, font_size, (0, 255, 0))
+        cv2.putText(disp, "br", (int(sorted_pts[3][0]), int(
+            sorted_pts[3][1])), font, font_size, (0, 255, 0))
+        # print(pts)
 
-    cv2.drawContours(image=disp, contours=[approx], contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
- 
-    cv2.imshow('plate_view', plate_view)
+        # resizing to have pairs of points
+        plate_view = self.transform_perspective(
+            CAR_WIDTH, CAR_HEIGHT, sorted_pts, out)
 
-    cv2.imshow('char 1', self.process_plate(0, plate_view))
-    cv2.imshow('char 2', self.process_plate(1, plate_view))
-    cv2.imshow('char 3', self.process_plate(2, plate_view))
-    cv2.imshow('char 4', self.process_plate(3, plate_view))
+        cv2.drawContours(image=disp, contours=[
+                         approx], contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
 
-    cv2.imshow('script_view', processed_im)
-    cv2.waitKey(3)
+        cv2.imshow('plate_view', plate_view)
 
-  
-  def process_plate(self, pos, plate_im):
-    """Crops and processes plate images for individual letter.
-    
-    Args: pos - the position in the license plate
-          plate_im - image of license plate
-          
-    Returns: processed image of plate"""
+        char_imgs = []
+        for i in range(4):
+          char_imgs.append(self.process_plate(i, plate_view))
 
-    crop = plate_im[PLATE_I:PLATE_F, int(pos*CAR_WIDTH/4):int((pos + 1)*CAR_WIDTH/4)]
-    resize = cv2.resize(crop, PLATE_RES)
+        cv2.imshow('char 1', char_imgs[0])
+        cv2.imwrite('/home/fizzer/ros_ws/src/ENPH353-Team12/src/license-plate-data/test_char_2.png', cv2.cvtColor(char_imgs[0], cv2.COLOR_BGR2GRAY))
+        cv2.imshow('char 2', char_imgs[1])
+        cv2.imshow('char 3', char_imgs[2])
+        cv2.imshow('char 4', char_imgs[3])
 
-    return resize
+        #print(char_reader.predict(cv2.cvtColor(char_imgs[0], cv2.COLOR_BGR2GRAY)))
 
+        cv2.imshow('script_view', processed_im)
+        cv2.waitKey(3)
 
-  
-  def transform_perspective(self, width, height, sorted_pts, image):
-    """Args: The coords of the polygon we are to transform into a rectangle.
-             Desired width and height of the transformed image.
-             The image from which we pull the polygon.
+    def process_plate(self, pos, plate_im):
+        """Crops and processes plate images for individual letter.
 
-             Returns: The polygon from the original image transformed into a square."""
-    pts = np.float32([[0, 0], [width, 0],
-                      [0, height], [width, height]])
-    Mat = cv2.getPerspectiveTransform(sorted_pts, pts)
-    return cv2.warpPerspective(image, Mat, (width, height))
+        Args: pos - the position in the license plate
+              plate_im - image of license plate
 
-  
-  def get_coords(self, contour):
-    """Args: Approximated contour extracted with CHAIN_APPROX_NONE (only the verticies)
-       Returns: List of verticies in (x,y) coords"""
-    i = 0
-    coords = []
-    for j in contour :
-        if(i % 2 == 0):
-            x = contour[i]
-            y = contour[i + 1]
-            coords.append((x,y))
-  
-        i = i + 1
-      
-    return coords
+        Returns: processed image of plate"""
 
+        crop = plate_im[PLATE_I:PLATE_F, int(
+            pos*CAR_WIDTH/4):int((pos + 1)*CAR_WIDTH/4)]
+        resize = cv2.resize(crop, PLATE_RES)
 
-  def contour_coords_sorted(self, list_of_points):
-    """Args: List of contour verticies
-       Returns: Verticies in list sorted by top to bottom, left to right"""
+        return resize
 
-    avg_y = 0
-    avg_x = 0
+    def transform_perspective(self, width, height, sorted_pts, image):
+        """Args: The coords of the polygon we are to transform into a rectangle.
+                 Desired width and height of the transformed image.
+                 The image from which we pull the polygon.
 
-    for i in list_of_points:
-      avg_y += i[1]
-      avg_x += i[0]
+                 Returns: The polygon from the original image transformed into a square."""
+        pts = np.float32([[0, 0], [width, 0],
+                          [0, height], [width, height]])
+        Mat = cv2.getPerspectiveTransform(sorted_pts, pts)
+        return cv2.warpPerspective(image, Mat, (width, height))
 
-    avg_y = int(avg_y/4)
-    avg_x = int(avg_x/4)
-    
+    def get_coords(self, contour):
+        """Args: Approximated contour extracted with CHAIN_APPROX_NONE (only the verticies)
+           Returns: List of verticies in (x,y) coords"""
+        i = 0
+        coords = []
+        for j in contour:
+            if (i % 2 == 0):
+                x = contour[i]
+                y = contour[i + 1]
+                coords.append((x, y))
 
-    for i in list_of_points:
-      if (int(i[1]) < avg_y and int(i[0]) < avg_x):
-        tl = i
-      elif (int(i[1]) < avg_y):
-        tr = i
-      elif (int(i[0]) < avg_x):
-        bl = i
-      else:
-        br = i
+            i = i + 1
 
-    coords = [list(tl), list(tr), list(bl), list(br)]
+        return coords
 
-    return np.float32(coords).reshape(-1, 2)
+    def contour_coords_sorted(self, list_of_points):
+        """Args: List of contour verticies
+           Returns: Verticies in list sorted by top to bottom, left to right"""
+
+        avg_y = 0
+        avg_x = 0
+
+        for i in list_of_points:
+            avg_y += i[1]
+            avg_x += i[0]
+
+        avg_y = int(avg_y/4)
+        avg_x = int(avg_x/4)
+
+        for i in list_of_points:
+            if (int(i[1]) < avg_y and int(i[0]) < avg_x):
+                tl = i
+            elif (int(i[1]) < avg_y):
+                tr = i
+            elif (int(i[0]) < avg_x):
+                bl = i
+            else:
+                br = i
+
+        coords = [list(tl), list(tr), list(bl), list(br)]
+
+        return np.float32(coords).reshape(-1, 2)
+
 
 def main(args):
-  ic = image_converter()
-  rospy.init_node('image_converter', anonymous=True)
-  try:
-    rospy.spin()
-  except KeyboardInterrupt:
-    print("Shutting down")
-  cv2.destroyAllWindows()
+    ic = image_converter()
+    rospy.init_node('image_converter', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main(sys.argv)
